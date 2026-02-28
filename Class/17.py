d457713f5c579f0143ed2c9878024a2cd06fa22c
AbaneeshPanigrahi <abaneeshpanigrahi@gmail.com>
2026-02-28 11:05:13 +0530

    Add files via upload

---
+++ Class/17.py
@@ -0,0 +1,41 @@
+#OOP-Object Oriented Programming
+#python is an object oriented programming language:-it is a versatile programming language that supports various programming styles,
+#including object-oriented programming (OOP). OOP is a programming paradigm that organizes code into objects, which are instances of classes. 
+#In OOP, you can define classes to represent real-world entities and create objects based on those classes. 
+# This allows for better code organization, reusability, and modularity.
+#An object is an entity that has attributes and behaviors. 
+# For example,a parrot is an object that has attributes like color and name, and behaviors like flying and talking.
+#Attributes-name,age,color,etc
+#behaviors-dancing,singing,etc
+#similarly, a class is a blueprint for that object.
+
+#OOP Concepts
+#1.class:-A class is a blueprint for creating objects. 
+# It defines a set of attributes and behaviors that the objects created from the class will have.
+
+#2.object:-An object is an instance of a class.
+# It is created based on the blueprint provided by the class and has its own unique set of attributes and behaviors.
+
+#3 polymorphism:-Polymorphism is the ability of different objects to respond to the same method or function call in different ways.
+
+#4.inheritance:-Inheritance is a fundamental concept in object-oriented programming that allows a new class (called a child or subclass) 
+#to inherit attributes and behaviors from an existing class (called a parent or superclass).
+
+#5.encapsulation:-Encapsulation is the concept of bundling data (attributes) and methods (behaviors) 
+#that operate on that data into a single unit, which is the class.
+
+#6.Data abstraction:-Data abstraction is the process of hiding the internal details of an object 
+#and exposing only the necessary information to the outside world.
+
+#Example of class and object
+# class Student:
+#     x=7
+#     y=8
+#     def fun1(self,name,age):
+#         self.name=name
+#         self.age=age
+        
+# s1=Student()
+# s1.fun1("Rohit",20)
+# print(s1.name,s1.age)
+
